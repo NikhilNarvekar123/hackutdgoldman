22eaa4cbf4fbe26b00f131449a113c9d08d043d7
Nikhil Narvekar <nikhil.narvekar.712@gmail.com>
2023-11-04 21:13:38 -0500

    merged

--- client/src/Company.js
+++ client/src/Company.js
@@ -1,11 +1,46 @@
 import Scores from "./Scores";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { Container, Stack } from '@chakra-ui/react';
+import Navbar from './Navbar';
+
+
+
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page A', uv: 300, pv: 2400, amt: 2400}, {name: 'Page A', uv: 200, pv: 2400, amt: 2400}, {name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+
+const renderLineChart = (
+    <LineChart width={600} height={300} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    </LineChart>
+);
 
 
 const Company = () => {
 
     return(
         <>
-        <Scores></Scores>
+        
+        <Navbar/>
+        <Container maxW={'5xl'}>
+        <Stack
+            textAlign={'center'}
+            align={'center'}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}>
+                
+                
+            
+                <Scores></Scores>
+                
+                
+                
+                
+                
+                {renderLineChart}
+        </Stack>
+        </Container>
         </>
     );
 }
